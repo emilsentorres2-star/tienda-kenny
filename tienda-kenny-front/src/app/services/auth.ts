be2c642731ce13de1bad4bb3bbb81f9a95f8b98c
emilsentorres2-star <emilsentorres2@gmail.com>
2026-04-22 06:02:14 -0500

    Intentando arreglar el AuthService

--- tienda-kenny-front/src/app/services/auth.ts
+++ tienda-kenny-front/src/app/services/auth.ts
@@ -6,9 +6,14 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
+  // Asegúrate de que esta URL sea la de tu backend
   private URL = 'http://localhost:3000/api/auth';
 
   constructor(private http: HttpClient) { }
+
+  registrar(usuario: any): Observable<any> {
+    return this.http.post(`${this.URL}/register`, usuario);
+  }
 
   login(usuario: any): Observable<any> {
     return this.http.post(`${this.URL}/login`, usuario);
